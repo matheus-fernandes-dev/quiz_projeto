c346846f3dd7388b12494edbb2104c02ac683920
Matheus Fernandes <matheusfernandes.trabalho@gmail.com>
2025-07-10 14:55:14 -0300

    Primeiro commit do projeto.

---
+++ perguntas.py
@@ -0,0 +1,85 @@
+
+#elaborando as perguntas
+perguntas = [
+
+    (
+        "Em quais dias da semana a coleta de lixo acontece em Pau Amarelo?",
+        ["Segunda, quarta e sexta",
+        "Terça, quinta e sábado",
+        "Domingo e segunda",
+        "Apenas aos sábados"],
+        "Terça, quinta e sábado",
+        "A prefeitura realiza a coleta de lixo nos dias de terça, quinta e sábado."
+    ),
+
+    (   
+        "Qual o periodo da coleta de lixo em Pau Amarelo?",
+        ["Noturno",
+        "Madrugada",
+        "Diurno",
+        "Vespertino"],
+        "Diurno",
+        "É importante colocar o lixo para fora próximo ao horário da manhã."
+    ),
+
+    (
+        "Por que é importante colocar o lixo para fora somente nos dias de coleta?",
+        ["Para evitar o acúmulo de lixo nas ruas e a presença de animais.",
+        "Para economizar sacos de lixo.",
+        "Para a prefeitura recolher menos lixo.",
+        "Porque os caminhões só passam uma vez por mês."],
+        "Para evitar o acúmulo de lixo nas ruas e a presença de animais.",
+        "O lixo deve ser colocado próximo ao horário da coleta"
+        "para evitar mau cheiro, acúmulo de lixo e atrair animais."
+    ),
+
+    (
+        "Descasquei a macaxeira, como devo jogar no lixo?", 
+        ["No mesmo saco onde separei o plástico.",
+        "No mesmo saco onde coloquei o vidro.",
+        "No mesmo saco com outros lixos orgânicos."],
+        "No mesmo saco com outros lixos orgânicos.", 
+        "As cascas devem ser separadas no mesmo saco ou lixeira para lixo orgânico."
+    ),
+
+   (
+        "Quebrei os pirex, como devo descartar o vidro quebrado?",
+        ["Jogar no lixo comum junto com restos de comida.",
+        "Embrulhar bem e sinalizar que contém vidro quebrado.",
+        "Misturar com papel e colocar na lixeira azul.",
+        "Guardar em casa, pois vidro não pode ser jogado fora."],
+        "Embrulhar bem e sinalizar que contém vidro quebrado.",
+        "Vidros quebrados devem ser embrulhados em papel grosso ou jornal e sinalizados para evitar acidentes."
+    ),
+
+   (
+        "Varri a casa e além de poeira, encontrei pedaços de plástico. Como devo descartar?",
+        ["Jogar tudo junto no saco de lixo comum, sem nenhum cuidado.",
+        "Separar o plástico em um saco.",
+        "Guardar o plástico em casa, pois não pode ser jogado fora.",
+        "Jogar tudo no vaso sanitário para não acumular."],
+        "Separar o plástico em um saco.",
+        "Separar materiais recicláveis em sacos diferentes evita a contaminação dos recicláveis com lixo orgânico."
+    ),
+
+    (
+        "Usei a camisa do deputado como pano de chão e ela desgastou. Como devo descartá-la?",
+        ["Jogar junto com restos de comida no lixo comum.",
+        "Queimar para não ocupar espaço.",
+        "Levar a um ponto de doação ou coleta para reaproveitamento.",
+        "Guardar em casa porque não pode descartar tecidos."],
+        "Levar a um ponto de doação ou coleta para reaproveitamento.",
+        "Roupas e tecidos não são recicláveis no lixo comum. Devem ser doadas, levadas a cooperativas ou pontos de coleta específicos para reaproveitamento ou reciclagem de tecidos."
+    ),
+
+    (
+        "O que devo fazer com as folhas secas que caem das plantas no meu quintal?",
+        ["Jogar junto com o lixo comum para ser coletado pela prefeitura.",
+        "Queimar as folhas secas para se livrar rapidamente delas.",
+        "Utilizar as folhas secas como compostagem ou adubo orgânico no próprio quintal.",
+        "Guardar as folhas secas dentro de casa para decoração."],
+        "Utilizar as folhas secas como compostagem ou adubo orgânico no próprio quintal.",
+        "Folhas secas podem ser reutilizadas para fazer compostagem, ajudando a devolver nutrientes ao solo e reduzir a quantidade de lixo."
+)
+
+]
---
+++ readme.md
@@ -0,0 +1,43 @@
+
+## 📝 Descrição
+Um quiz educativo para conscientizar os moradores do meu condomínio sobre o descarte correto do lixo.  
+De forma leve e divertida, o jogo ensina a separar resíduos corretamente e a respeitar os horários de coleta, evitando problemas comuns como lixo espalhado por animais ou resíduos misturados. O jogo salva os melhores jogadores em um ranking para incentivar todo mundo a melhorar a pontuação e competir de forma saudável.
+
+---
+
+## 🗑️ Desafios identificados
+
+### ♻️ Misturamos tudo num saco só
+Ainda temos o costume de jogar tudo junto: resto de comida, garrafa pet, caixa de papelão, pilha… tudo no mesmo saco.  
+Isso dificulta a coleta seletiva, impede a reciclagem do que poderia ser reaproveitado e ainda enche desnecessariamente os aterros sanitários.
+
+### ⏰ Colocamos o lixo fora na hora errada
+Muitas vezes deixamos o lixo na calçada fora do horário certo da coleta pública.  
+Resultado: sacos ficam horas ou dias expostos, e aí aparecem cães, gatos, urubus e até timbus rasgando tudo e espalhando o lixo.
+
+---
+
+## 💡 Solução proposta
+Criação de um quiz interativo e divertido, que ajuda a ensinar ou relembrar:
+- Como separar cada tipo de resíduo corretamente.
+- Por que respeitar o horário da coleta.
+
+A cada pergunta, você aprende algo novo, reflete sobre os erros e ainda pode disputar o ranking dos mais conscientes do prédio.
+
+---
+
+## 🤔 Por que fazer isso?
+Essa ideia nasceu da vontade de deixar a nossa rua mais limpa, bonita e organizada, e incentivar todo mundo aqui a ter atitudes mais sustentáveis no dia a dia.  
+Não é nada complicado: só mudar pequenos hábitos já faz uma diferença enorme.
+
+---
+
+## 👥 Público-alvo
+Esse projeto foi pensado para nós mesmos, moradores do condomínio.  
+Somos só cinco, mas cada um fazendo a sua parte ajuda a manter o nosso cantinho agradável, saudável e bom para todos.
+
+---
+
+## 🎮 Bora jogar, aprender e deixar a rua mais limpa?
+
+---
