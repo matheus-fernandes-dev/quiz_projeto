a8849a731605e410fc3e820fc1168a3eb393f086
Matheus Fernandes <matheusfernandes.trabalho@gmail.com>
2025-07-28 06:25:16 -0300

    Ranking do quiz.

--- ranking.py
+++ ranking.py
@@ -14,7 +14,7 @@ def salvar_ranking(nome, pontuacao):#FUNÇÃO PARA SALVAR A PONTUAÇÃO E ATUALI
     with open("ranking.json", "w", encoding="utf-8") as f: #GERANDO O ARQUIVO COM A INFORMAÇÕES DOS JOGADORES
         json.dump(ranking, f, indent=2, ensure_ascii=False)
 
-def carregar_ranking():#FUNÇÃO PARA CARREGAR AS INFORMAÇÕES DOS JOGADORES NO ARQUIVO E MOSTRAR
+def carregar_ranking():#FUNÇÃO PARA CARREGAR AS INFORMAÇÕES DOS JOGADORES NO ARQUIVO
     if os.path.exists("ranking.json"):
         with open("ranking.json", "r", encoding="utf-8") as f:
             return json.load(f)
